bbc54cf49ea0e48cee53295dfb037b7010cbb156
Aiga K <mikelis.kaneps@vialet.eu>
2023-10-10 14:17:10 +0300

    test 1

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android 5")
+                    Greeting("Android 6")
                 }
             }
         }
