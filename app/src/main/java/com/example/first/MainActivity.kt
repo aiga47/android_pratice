6ce7a3c88d95ddc1baf44042539172f3c89159ae
AK <mikelis.kaneps@vialet.eu>
2023-10-10 14:40:46 +0300

    test 6

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android 9")
+                    Greeting("Android 10")
                 }
             }
         }
