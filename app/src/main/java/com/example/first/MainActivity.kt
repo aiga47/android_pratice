5a8fb4b98a02d542a9f50cde9e8a1a2d04d02fd5
AK <aiga.kanepa@gmail.com>
2023-10-10 14:45:31 +0300

    test 7

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android 10")
+                    Greeting("Android 11")
                 }
             }
         }
